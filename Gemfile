9d855e199af34ece9cbcd9bb6aa11f7d154b2f68
Daniel Smilansky <dsmilansky@gmail.com>
2013-11-06 12:23:03 -0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+
+gem 'pg'
+
+gem 'haml'
+gem 'bootstrap-sass', '~> 3.0.0.0.rc'
+gem 'sass-rails', '~> 4.0.1'
+gem 'uglifier', '2.1.1'
+gem 'coffee-rails', '~> 4.0.1'
+gem 'jquery-rails', '3.0.4'
+gem 'turbolinks', '1.1.1'
+gem 'jbuilder', '~> 1.0.2'
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dsmilansky::Application.config.secret_key_base = 'cf8ace1a9fc9494756ba7fc168d01ad513f59a73908fae9bbd961dbf6b7202e4819142ba258aec70a8b5e5913fc0541c37f180d0c78177e20691ab495bdd3e38'
