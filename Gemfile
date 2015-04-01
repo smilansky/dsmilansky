25380207bfb47ddac0bfb6a0b51a7369848e9e36
Daniel Smilansky <dsmilansky@gmail.com>
2015-04-01 16:53:47 -0700

    new layout

--- Gemfile
+++ Gemfile
@@ -14,7 +14,6 @@ gem 'jquery-rails', '3.0.4'
 gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '~> 1.0.2'
 gem 'unicorn'
-gem 'blogdiggity', :git => 'git://github.com/integrallis/blogdiggity.git'
 
 group :doc do
   gem 'sdoc', '0.3.20', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Dsmilansky::Application.routes.draw do
   root to: 'pages#home'
 
-  mount Blogdiggity::Engine => "/blog", as: "blog"
-  
+
   get 'home', to: 'pages#home'
-  get 'projects', to: 'pages#projects'
+  get 'work', to: 'pages#work'
+  get 'contact', to: 'pages#contact'
 end
 
