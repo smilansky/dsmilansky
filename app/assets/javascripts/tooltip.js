fec82d57b10f3927e7529b231dcbd32a2944f7bb
Daniel Smilansky <dsmilansky@gmail.com>
2015-05-22 12:17:48 -0700

    update layout

---
+++ app/assets/javascripts/tooltip.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+$('[data-toggle="phone"]').tooltip(
+    {content: "(818)421-3054"}  
+
+    );
+$('[data-toggle="email"]').tooltip(
+    {content: "dsmilansky@gmail.com"}  
+
+    );
+
+});
