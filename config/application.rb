652daf0fd25c4a94ca5f68a206f1a98cc1f785a9
Daniel Smilansky <dsmilansky@gmail.com>
2015-11-29 11:14:05 -0800

    assets

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module Dsmilansky
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += ['blogdiggity/blogdiggity.css', 'blogdiggity/blogdiggity.js']
     config.assets.precompile << /\.(?:svg|eot|woff|ttf|otf)$/
     config.assets.enabled = true 
     config.assets.paths << "#{Rails.root}/app/assets/fonts" 
